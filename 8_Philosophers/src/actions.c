b59f9730f88e7299f9e7f5f4e147e37d62e2b751
Ramesh Timsina <rtimsina@c4a12c9.42berlin.de>
2023-11-09 15:34:48 +0100

    philo fixed

--- 8_Philosophers/src/actions.c
+++ 8_Philosophers/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: rtimsina <rtimsina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 11:16:34 by rtimsina          #+#    #+#             */
-/*   Updated: 2023/11/04 17:55:37 by rtimsina         ###   ########.fr       */
+/*   Updated: 2023/11/09 14:57:18 by rtimsina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,30 +31,56 @@ void	lone_philo(t_philo *philo)
 	return ;
 }
 
+int	philo_acquire_forks(t_philo *philo)
+{
+	if (philo->r_fork < philo->l_fork)
+	{
+		pthread_mutex_lock(philo->r_fork);
+		//ft_usleep(philo->time_to_eat / 2);
+		if (philo->l_fork)
+		{
+			pthread_mutex_lock(philo->l_fork);
+			print_msg("has taken a fork", philo, philo->id);
+			print_msg("has taken a fork", philo, philo->id);
+		}
+		else
+		{
+			pthread_mutex_unlock(philo->r_fork);
+			return (0);
+		}
+	}
+	else
+	{
+		pthread_mutex_lock(philo->l_fork);
+		if (philo->r_fork)
+		{
+		//ft_usleep(philo->time_to_eat / 2);
+			pthread_mutex_lock(philo->r_fork);
+			print_msg("has taken a fork", philo, philo->id);
+			print_msg("has taken a fork", philo, philo->id);
+		}
+		else
+		{
+			pthread_mutex_unlock(philo->l_fork);
+			return (0);
+		}
+	}
+	return (1);
+	//ft_usleep(50);
+}
 void	philo_eat(t_philo *philo)
 {
 	if (philo->num_of_philos == 1)
 		return (lone_philo(philo));
 	if (!philo->r_fork && !philo->l_fork)
 	{
-		ft_usleep(philo->time_to_eat / 2);
+		ft_usleep(philo->time_to_eat * 2);
 		return;
 	}
-	else if (philo->r_fork < philo->l_fork)
+	if (philo_acquire_forks(philo) == 0)
 	{
-		pthread_mutex_lock(philo->r_fork);
-		print_msg("has taken a fork", philo, philo->id);
-
-		pthread_mutex_lock(philo->l_fork);
-		print_msg("has taken a fork", philo, philo->id);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->l_fork);
-		print_msg("has taken a fork", philo, philo->id);
-
-		pthread_mutex_lock(philo->r_fork);
-		print_msg("has taken a fork", philo, philo->id);
+		ft_usleep(philo->time_to_eat * 2);
+		return;		
 	}
 	philo->eating = 1;
 	print_msg("is eating", philo, philo->id);
